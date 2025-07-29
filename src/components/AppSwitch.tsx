bc3b90052ce6a7ded025c760fdbd68be897989fc
a.abbaspour <a.abbaspour@HRBOX-PC-12>
2025-07-29 09:15:34 +0330

    added dynamic classes to AppSwitch.tsx

--- src/components/AppSwitch.tsx
+++ src/components/AppSwitch.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store.ts";
 
 export const AppSwitch = ({ props }: { props: any }) => {
-  const { isSelected, handleChange, children } = props;
+  const { isSelected, handleChange, children, classNames } = props;
 
   const lang = useSelector((state: RootState) => state.language.lang);
 
@@ -12,17 +12,22 @@ export const AppSwitch = ({ props }: { props: any }) => {
     <Switch
       classNames={{
         thumb: cn(
-          "shadow-lg !w-3 !h-3 transition-all duration-300 ease-in-out transform",
+          classNames.thumb +
+            " " +
+            "shadow-lg !w-3 !h-3 transition-all duration-300 ease-in-out transform",
           "dark:bg-info-1000 dark:shadow-lg",
           "group-data-[selected=true]:ms-2.5",
           "scale-100",
           isSelected && (lang === "en" ? "!mr-auto" : "!ml-auto"),
         ),
         wrapper: cn(
-          "!w-[30px] !h-[18px] rounded-full flex items-center justify-start transition-all duration-500 ease-in-out",
+          classNames.wrapper +
+            " " +
+            "!w-[30px] !h-[18px] rounded-full flex items-center justify-start transition-all duration-500 ease-in-out",
           "bg-neutral-200 text-info-1000 dark:bg-netural-100",
           "group-data-[selected=true]:bg-primary-400",
         ),
+        ...classNames,
       }}
       isSelected={isSelected}
       size="sm"
