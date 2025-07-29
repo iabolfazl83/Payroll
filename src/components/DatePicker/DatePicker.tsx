1361f63522967903b79e6a6711836eed32ba6dee
a.abbaspour <a.abbaspour@HRBOX-PC-12>
2025-07-29 09:34:26 +0330

    updated DatePicker.tsx.

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,15 +1,29 @@
 import { DatePicker } from "@heroui/react";
 import { useTranslation } from "react-i18next";
 
-export default function AppDatePicker() {
+export default function AppDatePicker({ props }: { props: any }) {
   const { t } = useTranslation();
+  const { label } = props;
 
   return (
     <div className="flex flex-col gap-1">
-      <label className="text-sm font-semibold text-gray-900">
-        Effective Date
+      <label className="text-sm font-semibold text-gray-900 dark:text-white">
+        {label}
       </label>
-      <DatePicker className="rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-primary-500" />
+      <DatePicker
+        calendarProps={{
+          classNames: {
+            cellButton: "dark:text-white",
+          },
+        }}
+        className="rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-primary-500"
+        classNames={{
+          segment: "dark:text-white",
+          popoverContent: "dark:text-white",
+        }}
+        label={t("effectiveDate")}
+        labelPlacement="inside"
+      />
     </div>
   );
 }
