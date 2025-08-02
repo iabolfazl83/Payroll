463db9b1819d733e12f450dd87d1143c29dd6a6e
a.abbaspour <a.abbaspour@HRBOX-PC-12>
2025-08-02 12:22:24 +0330

    fixed background color in dark mode.

--- src/components/AppSwitch.tsx
+++ src/components/AppSwitch.tsx
@@ -26,7 +26,7 @@ export const AppSwitch = ({ props }: { props: any }) => {
           " " +
           cn(
             "!w-[30px] !h-[18px] rounded-full flex items-center justify-start transition-all duration-500 ease-in-out",
-            "bg-neutral-200 text-info-1000 dark:bg-netural-100",
+            "bg-neutral-200 text-info-1000 dark:bg-netural-700",
             "group-data-[selected=true]:bg-primary-400",
           ),
         ...classNames,
