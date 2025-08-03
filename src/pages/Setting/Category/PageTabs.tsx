35f88b022d98988940a817633251a28e1618acf6
a.abbaspour <workgabolfazl83@gmail.com>
2025-08-04 00:40:00 +0330

    fixed Modal imports.

--- src/pages/Setting/Category/PageTabs.tsx
+++ src/pages/Setting/Category/PageTabs.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 import Search from "@/components/Search.tsx";
 import FilterModal from "@/components/FilterModal.tsx";
 import { Index as Icons } from "@/icons/Index.tsx";
-import ModalTypes from "@/pages/Setting/Types/ModalTypes.tsx";
+import ModalCategory from "@/pages/Setting/Category/ModalCategory.tsx";
 
 export default function PageTabs({ props }: { props: any }) {
   const { data } = props;
@@ -33,7 +33,7 @@ export default function PageTabs({ props }: { props: any }) {
         {t("addNewOne")}
       </Button>
 
-      <ModalTypes
+      <ModalCategory
         isEditMode={false}
         isOpen={isOpen}
         isShowMode={false}
